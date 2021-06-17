a47c32d30cb94da7d096f05f149ef31043eb40a7
jefedcreator <smallztin@gmail.com>
2021-06-18 00:59:16 +0100

    button component for dev challenges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from '@material-ui/core/Button';
+// import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="firstDiv">
+        <Button variant="contained">Default</Button>
+      </div>
+      <div className="firstDiv">
+        <Button variant="outlined" color="primary">
+          Primary
+        </Button>
+      </div>
+      <div className="firstDiv">
+        <Button color="primary">Primary</Button>
+      </div>
+      <div className="firstDiv">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+      </div>
+      <div className="firstDiv">
+        <Button variant="contained" disabled>
+          Disabled
+        </Button>
+      </div>
+      <div className="firstDiv">
+        <Button variant="contained" 
+        // startIcon={<AddShoppingCartIcon/>}
         >
-          Learn React
-        </a>
-      </header>
+          Default
+        </Button>
+        <Button variant="contained"
+        //  endIcon={<AddShoppingCartIcon/>}
+         >
+          Default
+        </Button>
+      </div>
+      <div className="firstDiv">
+        <Button variant="outlined" size="small" color="primary">
+          Small
+        </Button>
+        <Button variant="outlined" size="medium" color="primary">
+          Medium
+        </Button>
+        <Button variant="outlined" size="large" color="primary">
+          Large
+        </Button>
+      </div>
+      <div className="firstDiv">
+        <Button>Default</Button>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+      </div>
+      <div className="firstDiv">
+        <Button variant="contained">Default</Button>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+      </div>
     </div>
   );
 }
+
+// function Button(props) {
+//   return (
+//     <>
+//       <button type="button" variant={props.variant} color={props.color}>
+//         {props.Name}
+//       </button>
+//     </>
+//   );
+// }
 
 export default App;
